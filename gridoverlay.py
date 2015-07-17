f5e4a809052909b6abe73f15159cfe053d60caf4
tay <tay7.git@gmail.com>
2015-07-17 23:23:20 +0900

    remove unused import

--- gridoverlay.py
+++ gridoverlay.py
@@ -25,7 +25,6 @@ from qgis import core
 from gridpropertiesdialog import GridPropertiesDialog
 from gridpluginlayertype import GridPluginLayerType
 from gridpluginlayer import GridPluginLayer
-import resources
 
 class GridOverlay:
     '''
